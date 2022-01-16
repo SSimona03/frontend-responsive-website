3180c4b6f565a781da6512ce331a3cd49a799cbe
Simona Stroe <79479935+SSimona03@users.noreply.github.com>
2022-01-16 23:10:53 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './index.css'
+
+
+ReactDom.render(<App />, document.getElementById('root')); //the App component will be renderd inside the index.html  into a div with Id: root
