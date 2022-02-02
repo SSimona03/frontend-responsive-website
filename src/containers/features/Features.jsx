6f889624373077a1db093986c1acb56a604509c2
Simona Stroe <79479935+SSimona03@users.noreply.github.com>
2022-02-02 00:32:08 +0000

    footer

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./features.css";
 import Feature from "../../components/feature/Feature";
+import "./features.css";
 
 const featuresData = [
   {
@@ -21,27 +21,21 @@ const featuresData = [
   },
 ];
 
-const Features = () => {
-  return (
-    <div className="gpt3__features section__padding" id="features">
-      <div className="gpt3__features-heading">
-        <h1 className="gradient__text">
-          The Future is Now and You Just Need to Realize It. Step into Future
-          Today & Make it Happen.
-        </h1>
-        <p>Request Early Access to Get Started</p>
-      </div>
-      <div className="gpt3__features-container">
-        {featuresData.map((feature, index) => (
-          <Feature
-            title={feature.title}
-            text={feature.text}
-            key={feature.title + index}
-          />
-        ))}
-      </div>
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">
+        The Future is Now and You Just Need to Realize It. Step into Future
+        Today. & Make it Happen.
+      </h1>
+      <p>Request Early Access to Get Started</p>
     </div>
-  );
-};
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Features;
